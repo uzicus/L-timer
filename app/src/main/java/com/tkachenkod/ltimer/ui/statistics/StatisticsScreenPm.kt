adb59e02353a235ffbfe0290a5100d679b12668e
Denis Tkachenko <dt@mobileup.ru>
2018-12-18 21:21:24 +0500

    Statistics chart sort fix

--- app/src/main/java/com/tkachenkod/ltimer/ui/statistics/StatisticsScreenPm.kt
+++ app/src/main/java/com/tkachenkod/ltimer/ui/statistics/StatisticsScreenPm.kt
@@ -1,5 +1,6 @@
 package com.tkachenkod.ltimer.ui.statistics
 
+import com.tkachenkod.ltimer.entity.TaskWithTimeRecords
 import com.tkachenkod.ltimer.entity.TimeRecord
 import com.tkachenkod.ltimer.extension.inject
 import com.tkachenkod.ltimer.extension.sumByLong
@@ -18,8 +19,11 @@ class StatisticsScreenPm : BaseScreenPm() {
         YEAR
     }
 
+    private val filteredTasks = State<List<StatisticsTask>>()
+
     val period = State(Period.DAY)
-    val periodTasks = State<List<StatisticsTask>>()
+    val chartTasks = State<List<StatisticsTask>>()
+    val listTasks = State<List<StatisticsTask>>()
 
     val labelClicks = Action<Period>()
 
@@ -35,58 +39,74 @@ class StatisticsScreenPm : BaseScreenPm() {
             period.observable
         )
             .map { (tasks, period) ->
-                val filteredTasks = tasks
-                    .filter { taskWithTimeRecords ->
-                        taskWithTimeRecords.timeRecordsDuration != 0L
-                    }
-                    .map {
-                        val lastTime = it.timeRecords
-                            .map(TimeRecord::startTime)
-                            .sorted()
-                            .lastOrNull()
-
-                        val filteredTimeRecords = it.timeRecords.filter { timeRecord ->
-                            when (period) {
-                                Period.DAY -> {
-                                    lastTime?.minusDays(1)?.isBefore(timeRecord.startTime) ?: false
-                                }
-                                Period.WEEK -> {
-                                    lastTime?.minusWeeks(1)?.isBefore(timeRecord.startTime) ?: false
-                                }
-                                Period.MONTH -> {
-                                    lastTime?.minusMonths(1)?.isBefore(timeRecord.startTime)
-                                        ?: false
-                                }
-                                else -> {
-                                    lastTime?.minusYears(1)?.isBefore(timeRecord.startTime) ?: false
-                                }
-                            }
-                        }
-
-                        it.copy(timeRecords = filteredTimeRecords)
-                    }
-
-                val sumDuration = filteredTasks.sumByLong {
-                    it.timeRecords.sumByLong(TimeRecord::duration)
-                }
-
-                filteredTasks.map { taskWithTimeRecords ->
-                    StatisticsTask(
-                        name = taskWithTimeRecords.task.name,
-                        color = taskWithTimeRecords.task.color,
-                        durationInSecond = taskWithTimeRecords.timeRecordsDuration,
-                        percent = (taskWithTimeRecords.timeRecordsDuration.toFloat() / sumDuration.toFloat())
-                    )
-                }.sortedByDescending {
-                    it.durationInSecond
-                }
+                tasks.statisticsByPeriod(period) to tasks
             }
             .distinctUntilChanged()
-            .subscribe(periodTasks.consumer)
+            .subscribe { (filteredTasks, tasks) ->
+                val tasksSortByDayToIndex = tasks.statisticsByPeriod(Period.DAY)
+                    .mapIndexed { index, statisticsTask ->
+                        statisticsTask.name to index
+                    }
+                    .toMap()
+
+                val tasksSortByDay = filteredTasks.sortedBy {
+                    tasksSortByDayToIndex[it.name]
+                }
+
+                listTasks.consumer.accept(filteredTasks)
+                chartTasks.consumer.accept(tasksSortByDay)
+            }
             .untilDestroy()
 
         labelClicks.observable
             .subscribe(period.consumer)
             .untilDestroy()
+    }
+
+    private fun List<TaskWithTimeRecords>.statisticsByPeriod(period: Period): List<StatisticsTask> {
+        val filteredTasks = this
+            .filter { taskWithTimeRecords ->
+                taskWithTimeRecords.timeRecordsDuration != 0L
+            }
+            .map {
+                val lastTime = it.timeRecords
+                    .map(TimeRecord::startTime)
+                    .sorted()
+                    .lastOrNull()
+
+                val filteredTimeRecords = it.timeRecords.filter { timeRecord ->
+                    when (period) {
+                        Period.DAY -> {
+                            lastTime?.minusDays(1)?.isBefore(timeRecord.startTime) ?: false
+                        }
+                        Period.WEEK -> {
+                            lastTime?.minusWeeks(1)?.isBefore(timeRecord.startTime) ?: false
+                        }
+                        Period.MONTH -> {
+                            lastTime?.minusMonths(1)?.isBefore(timeRecord.startTime) ?: false
+                        }
+                        else -> {
+                            lastTime?.minusYears(1)?.isBefore(timeRecord.startTime) ?: false
+                        }
+                    }
+                }
+
+                it.copy(timeRecords = filteredTimeRecords)
+            }
+
+        val sumDuration = filteredTasks.sumByLong {
+            it.timeRecords.sumByLong(TimeRecord::duration)
+        }
+
+        return filteredTasks.map { taskWithTimeRecords ->
+            StatisticsTask(
+                name = taskWithTimeRecords.task.name,
+                color = taskWithTimeRecords.task.color,
+                durationInSecond = taskWithTimeRecords.timeRecordsDuration,
+                percent = (taskWithTimeRecords.timeRecordsDuration.toFloat() / sumDuration.toFloat())
+            )
+        }.sortedByDescending {
+            it.durationInSecond
+        }
     }
 }
