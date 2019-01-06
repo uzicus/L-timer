d1554b2b4b975c90ddef3d0435e1754a93e040c4
Denis Tkachenko <dt@mobileup.ru>
2019-01-06 23:09:31 +0500

    some fixes

--- app/src/main/java/com/tkachenkod/ltimer/extension/AppExtensions.kt
+++ app/src/main/java/com/tkachenkod/ltimer/extension/AppExtensions.kt
@@ -1,7 +1,10 @@
 package com.tkachenkod.ltimer.extension
 
+import android.app.ActivityManager
 import android.app.NotificationManager
+import android.app.Service
 import android.content.Context
+import androidx.core.content.getSystemService
 import org.koin.core.parameter.ParameterDefinition
 import org.koin.core.parameter.emptyParameterDefinition
 import org.koin.standalone.StandAloneContext
@@ -14,7 +17,15 @@ inline fun <reified T : Any> inject(
 }
 
 inline val Context.notificationManager: NotificationManager
-    get() = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    get() = getSystemService() ?: throw IllegalArgumentException(/* TODO */)
+
+inline val Context.activityManager: ActivityManager
+    get() = getSystemService() ?: throw IllegalArgumentException(/* TODO */)
+
+inline fun <reified T> Context.serviceIsRunning(): Boolean {
+    return activityManager.getRunningServices(Int.MAX_VALUE)
+        .any { it.service.className == T::class.java.name }
+}
 
 inline fun <T> Iterable<T>.sumByLong(selector: (T) -> Long): Long {
     var sum = 0L
---
+++ app/src/main/java/com/tkachenkod/ltimer/utils/RxBroadcastReceiver.kt
@@ -0,0 +1,48 @@
+package com.tkachenkod.ltimer.utils
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
+import java.util.concurrent.atomic.AtomicBoolean
+
+class RxBroadcastReceiver(
+    private val context: Context,
+    private val intentFilter: IntentFilter
+) : Observable<Intent>(), Disposable {
+
+    private var receiver: BroadcastReceiver? = null
+    private val disposed = AtomicBoolean(false)
+
+    override fun subscribeActual(observer: Observer<in Intent>) {
+        receiver = ReceiverDisposable(onReceive = { intent ->
+            if (isDisposed.not()) {
+                observer.onNext(intent)
+            }
+        })
+
+        context.registerReceiver(receiver, intentFilter)
+
+        observer.onSubscribe(this)
+    }
+
+    override fun isDisposed() = disposed.get()
+
+    override fun dispose() {
+        if (disposed.compareAndSet(false, true) && receiver != null) {
+            context.unregisterReceiver(receiver)
+        }
+    }
+
+    inner class ReceiverDisposable(val onReceive: (Intent) -> Unit): BroadcastReceiver() {
+
+        override fun onReceive(context: Context?, intent: Intent) {
+            onReceive.invoke(intent)
+        }
+
+    }
+
+}
